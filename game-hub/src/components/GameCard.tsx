1e695f419f3ac64589098c603e9c1a03da31c991
aman <amanuel.demirew@yahoo.com>
2024-10-01 17:41:50 +0300

    fetch optimized iamges

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -13,7 +13,7 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={'space-between'}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
