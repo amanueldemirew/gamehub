0fd1bd127f6d996dd0d2aeb37b58bc657b1d5831
amanueldemirew <amanuel.demirew@yahoo.com>
2024-10-02 21:34:09 +0300

    gitignore

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App";
-import theme from "./theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
--- src/hooks/usePlatform.tsx
+++
@@ -1,9 +0,0 @@
-import platform from "../data/platform";
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-const usePlatform = () => ({ data: platform, isloading: false, error: null });;
-
-export default usePlatform;
