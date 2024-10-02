c2a10da0c92882225a58cf5cdc2160454e2bca62
aman <amanuel.demirew@yahoo.com>
2024-10-02 15:18:11 +0300

    apply Emoji

---
+++ game-hub/src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { ImageProps, Image } from "@chakra-ui/react";
+import bullsEye from "../assets/bullseye.svg";
+import meh from "../assets/neutral-face.svg";
+import thumbup from "../assets/thumbs-up.svg";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: bullsEye, alt: "Recommended", boxSize: "35px" },
+    5: { src: thumbup, alt: "Exceptional", boxSize: "25px" },
+  };
+
+  return <Image {...emojiMap[rating]} mariginTop={1} />;
+};
+
+export default Emoji;
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -9,6 +9,8 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
+  rating: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
