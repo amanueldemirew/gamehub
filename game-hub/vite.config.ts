733e4806b1464ad468effbd9d238a35836e23522
amanueldemirew <amanuel.demirew@yahoo.com>
2024-10-03 10:13:21 +0300

    another hompage correction

--- game-hub/vite.config.ts
+++ game-hub/vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gamehub/game-hub/", // Must match the GitHub repo name
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
 });
